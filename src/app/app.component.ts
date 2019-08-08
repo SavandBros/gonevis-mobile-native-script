2992ff8a1747ee05e11cb40602b14884efacd73c
Arsalan Savand <arsalan@savandbros.com>
2019-08-08 10:46:09 +0300

    Update and fix AppComponent and sidebar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,73 +1,124 @@
+import { ItemEventData } from 'tns-core-modules/ui/list-view'
 import { AfterViewInit, ChangeDetectorRef, Component, OnInit, ViewChild } from '@angular/core';
-import { RadSideDrawerComponent } from 'nativescript-ui-sidedrawer/angular';
-import { RadSideDrawer } from 'nativescript-ui-sidedrawer';
-import { User, UserBlog } from '~/app/models/user/user';
-import { AuthService } from '~/app/services/auth/auth.service';
+import { UrlTree } from '@angular/router';
+import { BlogMinimalUser } from '@app/interfaces/blog-minimal-user';
+import { UserAuth } from '@app/interfaces/user-auth';
+import { BlogService } from '@app/services/blog/blog.service';
 import { RouterExtensions } from 'nativescript-angular';
-import { EntryStatuses } from '~/app/enums/entry_statuses/entry_statuses';
+import { ListViewEventData } from 'nativescript-ui-listview/ui-listview.common';
+import { RadSideDrawer } from 'nativescript-ui-sidedrawer';
+import { RadSideDrawerComponent } from 'nativescript-ui-sidedrawer/angular';
+import { AuthService } from '~/app/services/auth/auth.service';
 
 const dialogs = require('tns-core-modules/ui/dialogs');
 
 @Component({
   moduleId: module.id,
   selector: 'ns-app',
-  templateUrl: 'app.component.html'
+  templateUrl: 'app.component.html',
 })
 export class AppComponent implements OnInit, AfterViewInit {
-  @ViewChild(RadSideDrawerComponent) public drawerComponent: RadSideDrawerComponent;
+  /**
+   * Rad side drawer component reference
+   */
+  @ViewChild(RadSideDrawerComponent, { static: false }) drawerComponent: RadSideDrawerComponent;
+
+  /**
+   * Side drawer
+   */
   private drawer: RadSideDrawer;
-  public user: User;
-  public blog: UserBlog;
+
+  /**
+   * Authenticated user
+   */
+  public user: UserAuth;
+
+  /**
+   * Current blog
+   */
+  public blog: BlogMinimalUser;
+
+  /**
+   * App pages
+   */
   public appPages = [{
     title: 'Posts',
     url: 'posts',
-    icon: 'f550'
+    icon: 'f550',
   }, {
     title: 'Pages',
     url: 'pages',
-    icon: 'f15c'
+    icon: 'f15c',
   }];
 
-  constructor(private changeDetectorRef: ChangeDetectorRef, private router: RouterExtensions,
+  constructor(private changeDetectorRef: ChangeDetectorRef,
+              private router: RouterExtensions,
               private authService: AuthService) {
   }
 
-  ngOnInit() {
-    // Dynamically get current user's data.
-    this.authService.user.subscribe((user: User): void => {
+  ngOnInit(): void {
+    /**
+     * Subscribe to user changes
+     */
+    this.authService.user.subscribe((user: UserAuth): void => {
       this.user = user;
     });
-
-    // Dynamically get current blog's data.
-    this.authService.blog.subscribe((blog: UserBlog): void => {
+    /**
+     * Subscribe to blog changes
+     */
+    BlogService.blog.subscribe((blog: BlogMinimalUser): void => {
       this.blog = blog;
     });
   }
 
-  ngAfterViewInit() {
+  ngAfterViewInit(): void {
     this.drawer = this.drawerComponent.sideDrawer;
     this.changeDetectorRef.detectChanges();
   }
 
+  /**
+   * Get icon
+   *
+   * @param icon
+   */
   getIcon(icon: string): string {
     return String.fromCharCode(parseInt(icon, 16));
   }
 
-  onNavigationsTap() {
+  /**
+   * Navigate to page and close side drawer
+   *
+   * @param event List view event
+   */
+  onNavigationsTap(event: ListViewEventData): void {
+    this.router.navigate(['/dash', this.appPages[event.index].url]);
     this.drawer.closeDrawer();
   }
 
+  /**
+   * Logout
+   */
   logOut(): void {
     dialogs.confirm({
       title: 'Logout',
       message: 'Are you sure you want to logout from your account?',
       okButtonText: 'Logout',
-      cancelButtonText: 'No'
+      cancelButtonText: 'No',
     }).then((result: boolean): void => {
       if (result) {
         this.authService.unAuth();
         this.drawer.closeDrawer();
       }
     });
+  }
+
+  /**
+   * @param route Route URL
+   *
+   * @return Boolean which determines whether given route is currently active or not
+   */
+  isRouteActive(route: string): boolean {
+    const urlTree: UrlTree = this.router.router.createUrlTree(['dash', route]);
+    return this.router.router.isActive(urlTree, false);
   }
 }
