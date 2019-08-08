01e3ff4123a33ae9eb4234dca874af33724ec2ba
Arsalan Savand <arsalan@savandbros.com>
2019-08-08 11:22:03 +0300

    Update guards and implement CanLoad instead of CanActivate

--- src/app/services/auth-guard/anonymous-guard.service.ts
+++ src/app/services/auth-guard/anonymous-guard.service.ts
@@ -1,22 +1,38 @@
 import { Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot } from '@angular/router';
-import { AuthService } from '../auth/auth.service';
+import { Router, CanLoad, UrlSegment, Route } from '@angular/router';
+import { AuthService } from '@app/services/auth/auth.service';
+import { Observable } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
-export class AnonymousGuardService implements CanActivate {
+export class AnonymousGuardService implements CanLoad {
 
   constructor(private router: Router, private authService: AuthService) {
   }
 
-  async canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
-    // Not logged in so return true
-    if (!this.authService.isAuth) {
+  /**
+   * A guard deciding if children can be loaded.
+   *
+   * @param route A configuration object that defines a single route.
+   *              A set of routes are collected in a `Routes` array to define a `Router` configuration.
+   *              The router attempts to match segments of a given URL against each route,
+   *              using the configuration options defined in this object.
+   *
+   *              Supports static, parameterized, redirect, and wildcard routes, as well as
+   *              custom route data and resolve methods.
+   * @param segments Represents list of URL segments.
+   *
+   *                 A UrlSegment is a part of a URL between the two slashes. It contains a path and the matrix
+   *                 parameters associated with the segment.
+   */
+  canLoad(route: Route, segments: UrlSegment[]): Observable<boolean> | Promise<boolean> | boolean {
+    // If user is not logged-in, then allow user to access current route.
+    if (!this.authService.isAuth()) {
       return true;
     }
 
-    // Logged in so prevent route from changing.
+    // Not logged-in prevent user from accessing current route and redirect user to dashboard page with the return url.
     this.router.navigateByUrl('dash');
     return false;
   }
--- src/app/services/auth-guard/auth-guard.service.ts
+++ src/app/services/auth-guard/auth-guard.service.ts
@@ -1,31 +1,40 @@
 import { Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot, CanActivate, CanActivateChild, Router, RouterStateSnapshot } from '@angular/router';
-import { AuthService } from '../auth/auth.service';
+import { Router, UrlSegment, Route, CanLoad } from '@angular/router';
+import { AuthService } from '@app/services/auth/auth.service';
+import { Observable } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
-export class AuthGuardService implements CanActivate, CanActivateChild {
+export class AuthGuardService implements CanLoad {
 
-  constructor(private router: Router, private authService: AuthService) {
+  constructor(private router: Router,
+              private authService: AuthService) {
   }
 
-  async checkPermission() {
+  /**
+   * A guard deciding if children can be loaded.
+   *
+   * @param route A configuration object that defines a single route.
+   *              A set of routes are collected in a `Routes` array to define a `Router` configuration.
+   *              The router attempts to match segments of a given URL against each route,
+   *              using the configuration options defined in this object.
+   *
+   *              Supports static, parameterized, redirect, and wildcard routes, as well as
+   *              custom route data and resolve methods.
+   * @param segments Represents list of URL segments.
+   *
+   *                 A UrlSegment is a part of a URL between the two slashes. It contains a path and the matrix
+   *                 parameters associated with the segment.
+   */
+  canLoad(route: Route, segments: UrlSegment[]): Observable<boolean> | Promise<boolean> | boolean {
     // If user is logged-in, then allow user to access current route.
-    if (this.authService.isAuth) {
+    if (this.authService.isAuth()) {
       return true;
     }
 
     // Not logged-in prevent user from accessing current route and redirect user to login page with the return url.
     this.router.navigateByUrl('login');
     return false;
-  }
-
-  async canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
-    return this.checkPermission();
-  }
-
-  async canActivateChild(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
-    return this.checkPermission();
   }
 }
