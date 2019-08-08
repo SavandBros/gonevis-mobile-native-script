8980b62519127a24b84e7db5fc0d34300a80df14
Arsalan Savand <arsalan@savandbros.com>
2019-08-08 11:25:34 +0300

    Update JwtInterceptorService

--- src/app/services/jwt-interceptor/jwt-interceptor.service.ts
+++ src/app/services/jwt-interceptor/jwt-interceptor.service.ts
@@ -1,38 +1,46 @@
 import { Injectable } from '@angular/core';
 import { HttpErrorResponse, HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from '@angular/common/http';
+import { HttpErrorResponseApi } from '@app/models/http-error-response-api';
+import { AuthService } from '@app/services/auth/auth.service';
 import { Observable, throwError } from 'rxjs';
 import { catchError } from 'rxjs/operators';
-import { AuthService } from '../auth/auth.service';
-import { SecureStorage } from 'nativescript-secure-storage';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class JwtInterceptorService implements HttpInterceptor {
-  secureStorage: SecureStorage = new SecureStorage();
 
   constructor(private authService: AuthService) {
   }
 
-  intercept(request: HttpRequest<void>, next: HttpHandler): Observable<HttpEvent<any>> {
-    let token: string;
-    // Get token from token subject.
-    token = this.secureStorage.getSync({ key: 'token' });
+  /**
+   * @param request The outgoing request to handle
+   * @param next The next interceptor in the chain, or the backend if no interceptors in the chain.
+   */
+  intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    const token: string = this.authService.getToken();
     // Add authorization header with bearer token if available.
     if (token) {
       request = request.clone({
         setHeaders: {
-          Authorization: `JWT ${token}`
-        }
+          Authorization: `JWT ${token}`,
+        },
       });
     }
-
     return next.handle(request).pipe(catchError((error: HttpErrorResponse): Observable<never> => {
-      // Auto logout if 401 response returned from api
-      if (error.status === 401) {
-        this.authService.unAuth();
+      if (error.error instanceof HttpErrorResponse) {
+        // A client-side or network error occurred. Handle it accordingly.
+        console.error('An error occurred:', error.error.message);
+      } else {
+        // Sign out if 401 response
+        if (error.status === 401) {
+          this.authService.unAuth();
+        }
+        // The backend returned an unsuccessful response code.
+        // The response body may contain clues as to what went wrong,
+        console.error('Backend returned code:', error.status, 'body was: ', error.error);
       }
-      return throwError(error);
+      return throwError(new HttpErrorResponseApi(error));
     }));
   }
 }
