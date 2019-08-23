40d9f130c6054539688b24dc860ccbf2e6cd8925
Arsalan Savand <arsalan@savandbros.com>
2019-08-23 12:51:40 +0300

    Update main routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,29 +1,26 @@
 import { NgModule } from '@angular/core';
-import { Routes } from '@angular/router';
+import { Routes, PreloadAllModules } from '@angular/router';
 import { NativeScriptRouterModule } from 'nativescript-angular/router';
 import { AnonymousGuardService } from '~/app/services/auth-guard/anonymous-guard.service';
 import { AuthGuardService } from '~/app/services/auth-guard/auth-guard.service';
 
 const routes: Routes = [{
-    path: '',
-    redirectTo: 'login',
-    pathMatch: 'full'
+  path: '',
+  redirectTo: 'login',
+  pathMatch: 'full',
 }, {
-    path: 'dash',
-    loadChildren: '~/app/components/dash/dash.module#DashModule',
-    canLoad: [AuthGuardService]
+  path: 'dash',
+  loadChildren: '~/app/components/dash/dash.module#DashModule',
+  canLoad: [AuthGuardService],
 }, {
-    path: 'login',
-    loadChildren: '~/app/components/login/login.module#LoginModule',
-    canLoad: [AnonymousGuardService]
-}, {
-    path: 'home',
-    loadChildren: '~/app/home/home.module#HomeModule'
+  path: 'login',
+  loadChildren: '~/app/components/login/login.module#LoginModule',
+  canLoad: [AnonymousGuardService],
 }];
 
 @NgModule({
-    imports: [NativeScriptRouterModule.forRoot(routes)],
-    exports: [NativeScriptRouterModule]
+  imports: [NativeScriptRouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })],
+  exports: [NativeScriptRouterModule],
 })
 export class AppRoutingModule {
 }
